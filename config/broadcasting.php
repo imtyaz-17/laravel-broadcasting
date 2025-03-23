36c78bfb8c071c1fb5366bd4aabb743182648277
imtyaz <imtiyaz@itconquest.com>
2025-03-23 22:38:21 +0600

    websocket BE setup

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -38,6 +38,9 @@ return [
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'useTLS' => true,
+                'host' => env('PUSHER_HOST', '127.0.0.1'),
+                'port' => env('PUSHER_PORT', 6001),
+                'scheme' => env('PUSHER_SCHEME', 'http'),
             ],
         ],
 
